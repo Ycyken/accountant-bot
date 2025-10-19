9f974d46cadc658b1db77a2cd1b5524084c22184
Gleb Nasretdinov <gleb.nasretdinov@proton.me>
2025-10-19 19:44:05 +0400

    feat: add db scheme and generate db layer

--- docs/saldo.sql
+++ docs/saldo.sql
@@ -1,6 +1,6 @@
 ﻿-- =============================================================================
 -- Diagram Name: saldo
--- Created on: 2025-10-19
+-- Created on: 10/19/2025 7:41:58 PM
 -- Diagram Version: 
 -- =============================================================================
 
@@ -13,7 +13,7 @@ CREATE TABLE "statuses" (
 );
 
 CREATE TABLE "users" (
-	"userId" SERIAL NOT NULL,
+	"userId" int4 NOT NULL,
 	"login" varchar(64) NOT NULL,
 	"password" varchar(64) NOT NULL,
 	"authKey" varchar(32),
@@ -28,11 +28,71 @@ CREATE INDEX "IX_FK_users_statusId_users" ON "users" USING BTREE (
 );
 
 
+CREATE TABLE "expenses" (
+	"expenseId" int4 NOT NULL GENERATED ALWAYS AS IDENTITY,
+	"userId" int4 NOT NULL,
+	"categoryId" int4,
+	"amount" int4 NOT NULL,
+	"description" text NOT NULL,
+	"createdAt" timestamp with time zone NOT NULL DEFAULT NOW(),
+	"updatedAt" timestamp with time zone NOT NULL DEFAULT NOW(),
+	"statusId" int4 NOT NULL,
+	"currency" varchar(12) NOT NULL,
+	PRIMARY KEY("expenseId")
+);
+
+CREATE TABLE "categories" (
+	"categoryId" int4 NOT NULL GENERATED ALWAYS AS IDENTITY,
+	"userId" int4 NOT NULL,
+	"title" text NOT NULL,
+	"alias" text,
+	"createdAt" timestamp with time zone NOT NULL DEFAULT NOW(),
+	"updatedAt" timestamp with time zone NOT NULL DEFAULT NOW(),
+	"statusId" int4 NOT NULL,
+	PRIMARY KEY("categoryId")
+);
+
+
 ALTER TABLE "users" ADD CONSTRAINT "FK_users_statusId" FOREIGN KEY ("statusId")
 	REFERENCES "statuses"("statusId")
 	MATCH SIMPLE
 	ON DELETE RESTRICT
 	ON UPDATE RESTRICT
+	NOT DEFERRABLE;
+
+ALTER TABLE "expenses" ADD CONSTRAINT "Ref_expenses_to_statuses" FOREIGN KEY ("statusId")
+	REFERENCES "statuses"("statusId")
+	MATCH SIMPLE
+	ON DELETE RESTRICT
+	ON UPDATE RESTRICT
+	NOT DEFERRABLE;
+
+ALTER TABLE "expenses" ADD CONSTRAINT "Ref_expenses_to_users" FOREIGN KEY ("userId")
+	REFERENCES "users"("userId")
+	MATCH SIMPLE
+	ON DELETE NO ACTION
+	ON UPDATE NO ACTION
+	NOT DEFERRABLE;
+
+ALTER TABLE "expenses" ADD CONSTRAINT "Ref_expenses_to_categories" FOREIGN KEY ("categoryId")
+	REFERENCES "categories"("categoryId")
+	MATCH SIMPLE
+	ON DELETE NO ACTION
+	ON UPDATE NO ACTION
+	NOT DEFERRABLE;
+
+ALTER TABLE "categories" ADD CONSTRAINT "Ref_categories_to_statuses" FOREIGN KEY ("statusId")
+	REFERENCES "statuses"("statusId")
+	MATCH SIMPLE
+	ON DELETE NO ACTION
+	ON UPDATE NO ACTION
+	NOT DEFERRABLE;
+
+ALTER TABLE "categories" ADD CONSTRAINT "Ref_categories_to_users" FOREIGN KEY ("userId")
+	REFERENCES "users"("userId")
+	MATCH SIMPLE
+	ON DELETE NO ACTION
+	ON UPDATE NO ACTION
 	NOT DEFERRABLE;
 
 
--- pkg/db/common.go
+++ pkg/db/common.go
@@ -20,13 +20,19 @@ func NewCommonRepo(db orm.DB) CommonRepo {
 	return CommonRepo{
 		db: db,
 		filters: map[string][]Filter{
-			Tables.User.Name: {StatusFilter},
+			Tables.User.Name:     {StatusFilter},
+			Tables.Category.Name: {StatusFilter},
+			Tables.Expense.Name:  {StatusFilter},
 		},
 		sort: map[string][]SortField{
-			Tables.User.Name: {{Column: Columns.User.CreatedAt, Direction: SortDesc}},
+			Tables.User.Name:     {{Column: Columns.User.CreatedAt, Direction: SortDesc}},
+			Tables.Category.Name: {{Column: Columns.Category.CreatedAt, Direction: SortDesc}},
+			Tables.Expense.Name:  {{Column: Columns.Expense.CreatedAt, Direction: SortDesc}},
 		},
 		join: map[string][]string{
-			Tables.User.Name: {TableColumns},
+			Tables.User.Name:     {TableColumns},
+			Tables.Category.Name: {TableColumns, Columns.Category.User},
+			Tables.Expense.Name:  {TableColumns, Columns.Expense.User, Columns.Expense.Category},
 		},
 	}
 }
@@ -124,4 +130,156 @@ func (cr CommonRepo) DeleteUser(ctx context.Context, id int) (deleted bool, err 
 	user := &User{ID: id, StatusID: StatusDeleted}
 
 	return cr.UpdateUser(ctx, user, WithColumns(Columns.User.StatusID))
+}
+
+/*** Category ***/
+
+// FullCategory returns full joins with all columns
+func (cr CommonRepo) FullCategory() OpFunc {
+	return WithColumns(cr.join[Tables.Category.Name]...)
+}
+
+// DefaultCategorySort returns default sort.
+func (cr CommonRepo) DefaultCategorySort() OpFunc {
+	return WithSort(cr.sort[Tables.Category.Name]...)
+}
+
+// CategoryByID is a function that returns Category by ID(s) or nil.
+func (cr CommonRepo) CategoryByID(ctx context.Context, id int, ops ...OpFunc) (*Category, error) {
+	return cr.OneCategory(ctx, &CategorySearch{ID: &id}, ops...)
+}
+
+// OneCategory is a function that returns one Category by filters. It could return pg.ErrMultiRows.
+func (cr CommonRepo) OneCategory(ctx context.Context, search *CategorySearch, ops ...OpFunc) (*Category, error) {
+	obj := &Category{}
+	err := buildQuery(ctx, cr.db, obj, search, cr.filters[Tables.Category.Name], PagerTwo, ops...).Select()
+
+	if errors.Is(err, pg.ErrMultiRows) {
+		return nil, err
+	} else if errors.Is(err, pg.ErrNoRows) {
+		return nil, nil
+	}
+
+	return obj, err
+}
+
+// CategoriesByFilters returns Category list.
+func (cr CommonRepo) CategoriesByFilters(ctx context.Context, search *CategorySearch, pager Pager, ops ...OpFunc) (categories []Category, err error) {
+	err = buildQuery(ctx, cr.db, &categories, search, cr.filters[Tables.Category.Name], pager, ops...).Select()
+	return
+}
+
+// CountCategories returns count
+func (cr CommonRepo) CountCategories(ctx context.Context, search *CategorySearch, ops ...OpFunc) (int, error) {
+	return buildQuery(ctx, cr.db, &Category{}, search, cr.filters[Tables.Category.Name], PagerOne, ops...).Count()
+}
+
+// AddCategory adds Category to DB.
+func (cr CommonRepo) AddCategory(ctx context.Context, category *Category, ops ...OpFunc) (*Category, error) {
+	q := cr.db.ModelContext(ctx, category)
+	if len(ops) == 0 {
+		q = q.ExcludeColumn(Columns.Category.CreatedAt)
+	}
+	applyOps(q, ops...)
+	_, err := q.Insert()
+
+	return category, err
+}
+
+// UpdateCategory updates Category in DB.
+func (cr CommonRepo) UpdateCategory(ctx context.Context, category *Category, ops ...OpFunc) (bool, error) {
+	q := cr.db.ModelContext(ctx, category).WherePK()
+	if len(ops) == 0 {
+		q = q.ExcludeColumn(Columns.Category.ID, Columns.Category.CreatedAt)
+	}
+	applyOps(q, ops...)
+	res, err := q.Update()
+	if err != nil {
+		return false, err
+	}
+
+	return res.RowsAffected() > 0, err
+}
+
+// DeleteCategory set statusId to deleted in DB.
+func (cr CommonRepo) DeleteCategory(ctx context.Context, id int) (deleted bool, err error) {
+	category := &Category{ID: id, StatusID: StatusDeleted}
+
+	return cr.UpdateCategory(ctx, category, WithColumns(Columns.Category.StatusID))
+}
+
+/*** Expense ***/
+
+// FullExpense returns full joins with all columns
+func (cr CommonRepo) FullExpense() OpFunc {
+	return WithColumns(cr.join[Tables.Expense.Name]...)
+}
+
+// DefaultExpenseSort returns default sort.
+func (cr CommonRepo) DefaultExpenseSort() OpFunc {
+	return WithSort(cr.sort[Tables.Expense.Name]...)
+}
+
+// ExpenseByID is a function that returns Expense by ID(s) or nil.
+func (cr CommonRepo) ExpenseByID(ctx context.Context, id int, ops ...OpFunc) (*Expense, error) {
+	return cr.OneExpense(ctx, &ExpenseSearch{ID: &id}, ops...)
+}
+
+// OneExpense is a function that returns one Expense by filters. It could return pg.ErrMultiRows.
+func (cr CommonRepo) OneExpense(ctx context.Context, search *ExpenseSearch, ops ...OpFunc) (*Expense, error) {
+	obj := &Expense{}
+	err := buildQuery(ctx, cr.db, obj, search, cr.filters[Tables.Expense.Name], PagerTwo, ops...).Select()
+
+	if errors.Is(err, pg.ErrMultiRows) {
+		return nil, err
+	} else if errors.Is(err, pg.ErrNoRows) {
+		return nil, nil
+	}
+
+	return obj, err
+}
+
+// ExpensesByFilters returns Expense list.
+func (cr CommonRepo) ExpensesByFilters(ctx context.Context, search *ExpenseSearch, pager Pager, ops ...OpFunc) (expenses []Expense, err error) {
+	err = buildQuery(ctx, cr.db, &expenses, search, cr.filters[Tables.Expense.Name], pager, ops...).Select()
+	return
+}
+
+// CountExpenses returns count
+func (cr CommonRepo) CountExpenses(ctx context.Context, search *ExpenseSearch, ops ...OpFunc) (int, error) {
+	return buildQuery(ctx, cr.db, &Expense{}, search, cr.filters[Tables.Expense.Name], PagerOne, ops...).Count()
+}
+
+// AddExpense adds Expense to DB.
+func (cr CommonRepo) AddExpense(ctx context.Context, expense *Expense, ops ...OpFunc) (*Expense, error) {
+	q := cr.db.ModelContext(ctx, expense)
+	if len(ops) == 0 {
+		q = q.ExcludeColumn(Columns.Expense.CreatedAt)
+	}
+	applyOps(q, ops...)
+	_, err := q.Insert()
+
+	return expense, err
+}
+
+// UpdateExpense updates Expense in DB.
+func (cr CommonRepo) UpdateExpense(ctx context.Context, expense *Expense, ops ...OpFunc) (bool, error) {
+	q := cr.db.ModelContext(ctx, expense).WherePK()
+	if len(ops) == 0 {
+		q = q.ExcludeColumn(Columns.Expense.ID, Columns.Expense.CreatedAt)
+	}
+	applyOps(q, ops...)
+	res, err := q.Update()
+	if err != nil {
+		return false, err
+	}
+
+	return res.RowsAffected() > 0, err
+}
+
+// DeleteExpense set statusId to deleted in DB.
+func (cr CommonRepo) DeleteExpense(ctx context.Context, id int) (deleted bool, err error) {
+	expense := &Expense{ID: id, StatusID: StatusDeleted}
+
+	return cr.UpdateExpense(ctx, expense, WithColumns(Columns.Expense.StatusID))
 }
