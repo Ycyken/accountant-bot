558e20e27f5670eaa693098d91bc0c27b793835c
Gleb Nasretdinov <gleb.nasretdinov@proton.me>
2025-10-22 17:20:50 +0300

    fix: whisper cmds run for now  with given ctx

--- pkg/saldo/whisper.go
+++ pkg/saldo/whisper.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
-	"time"
 )
 
 type Whisper struct{}
@@ -14,35 +13,28 @@ func (w *Whisper) Transcribe(ctx context.Context, audioFilePath string) (string,
 	tmpWav := "tmp.wav"
 	defer os.Remove(tmpWav)
 
-	output, err := execWithTimeout(2*time.Second, "ffmpeg",
+	cmd := exec.CommandContext(ctx, "ffmpeg",
 		"-y", // overwrite output file without asking
 		"-i", audioFilePath,
 		"-ac", "1", // 1 channel
 		"-ar", "16000", // 16 kHz
 		"-acodec", "pcm_s16le", // 16-bit little-endian PCM
 		tmpWav)
+	output, err := cmd.CombinedOutput()
 	if err != nil {
 		return "", fmt.Errorf("ffmpeg error: %w, output: %s", err, string(output))
 	}
 
-	output, err = execWithTimeout(7*time.Second,
+	cmd = exec.CommandContext(ctx,
 		"whisper-cli",
 		"-f", tmpWav,
 		"-otxt",
 		"-of", "-",
 	)
+	output, err = cmd.CombinedOutput()
 	if err != nil {
 		return "", fmt.Errorf("whisper-cli error: %w, output: %s", err, string(output))
 	}
 
 	return string(output), nil
-}
-
-func execWithTimeout(timeout time.Duration, name string, args ...string) ([]byte, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), timeout)
-	defer cancel()
-
-	cmd := exec.CommandContext(ctx, name, args...)
-	out, err := cmd.CombinedOutput()
-	return out, err
 }
