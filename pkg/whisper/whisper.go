85911eb7206a45bd1416ef036391fff278bb7a7f
Gleb Nasretdinov <gleb.nasretdinov@proton.me>
2025-10-22 16:10:33 +0300

    ref: linter

--- pkg/whisper/whisper.go
+++ pkg/whisper/whisper.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"time"
 )
 
 type Whisper struct{}
@@ -12,31 +13,36 @@ type Whisper struct{}
 func (w *Whisper) Transcribe(ctx context.Context, audioFilePath string) (string, error) {
 	tmpWav := "tmp.wav"
 	defer os.Remove(tmpWav)
-	cmd := exec.Command(
-		"ffmpeg",
+
+	output, err := execWithTimeout(2*time.Second, "ffmpeg",
 		"-y", // overwrite output file without asking
 		"-i", audioFilePath,
 		"-ac", "1", // 1 channel
 		"-ar", "16000", // 16 kHz
 		"-acodec", "pcm_s16le", // 16-bit little-endian PCM
-		tmpWav,
-	)
-	output, err := cmd.CombinedOutput()
+		tmpWav)
 	if err != nil {
-		return "", fmt.Errorf("ffmpeg error: %v, output: %s", err, string(output))
+		return "", fmt.Errorf("ffmpeg error: %w, output: %s", err, string(output))
 	}
 
-	cmd = exec.Command(
+	output, err = execWithTimeout(7*time.Second,
 		"whisper-cli",
 		"-f", tmpWav,
 		"-otxt",
 		"-of", "-",
 	)
-
-	output, err = cmd.CombinedOutput()
 	if err != nil {
-		return "", fmt.Errorf("whisper-cli error: %v, output: %s", err, string(output))
+		return "", fmt.Errorf("whisper-cli error: %w, output: %s", err, string(output))
 	}
 
 	return string(output), nil
+}
+
+func execWithTimeout(timeout time.Duration, name string, args ...string) ([]byte, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	cmd := exec.CommandContext(ctx, name, args...)
+	out, err := cmd.CombinedOutput()
+	return out, err
 }
